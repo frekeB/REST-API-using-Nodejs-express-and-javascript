8aba785ffd833052df0c78b7ddfdbcf95fd60c55
Ndifreke Etukudo <ndifreke.etukudo@decagon.dev>
2022-12-28 08:58:05 +0100

    final commit

--- app.js
+++ app.js
@@ -1,41 +1,86 @@
-const express = require('express');
+const { request } = require("express");
+const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const movies = [
-    {
-        id: 1,
-        name:"Tenet",
-    },
-    {
-        id: 2,
-        name:"Inception",
-    },
-    {
-        id: 3,
-        name:"Avengers",
-    },
-    {
-        id: 4,
-        name:"Big Hero 6",
-    },
+  {
+    id: 1,
+    name: "Tenet",
+  },
+  {
+    id: 2,
+    name: "Inception",
+  },
+  {
+    id: 3,
+    name: "Avengers",
+  },
+  {
+    id: 4,
+    name: "Big Hero 6",
+  },
+];
+
+const getMovies = (req, res) => {
+  res.send(movies);
+};
+const getmovie = (req, res) => {
+  const movie = movies.find((movie) => movie.id === parseInt(req.params.id));
+  if (!movie) return res.status(404).send({ error: "Reource not found" });
+
+  res.send(movie);
+};
+const addMovie = (req, res) => {
+  const name = req.body.name;
+
+  const movie = {
+    id: movies.length + 1,
+    name,
+  };
+
+  movies.push(movie);
+  res.send(movie);
+};
+
+const updateMovie = (req, res) => {
+  const movie = movies.find((movie) => movie.id === parseInt(req.params.id));
+  if (!movie) return res.status(404).send({ error: "Reource not found" });
+
+  movie.name = req.body.name;
+  res.send(movie);
+};
+const deleteMovie = (req, res) => {
+    const movie = movies.find((movie) => movie.id === parseInt(req.params.id));
+    if (!movie) {
+      return res.status(404).send({ error: "Resource not found" });
+    }
+    const index = movies.indexOf(movie);
+    movies.splice(index, 1);
+  
+    res.send(movie);
+  }
 
 
-]
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-const getMovies =(req, res)=>{
-    res.send(movies);
-}
+app.get("/api/movies", getMovies);
 
-app.get('/', (req, res)=>{
-    res.send("hello world");
-})
+app.get("/api/movies/:id", getmovie);
 
-app.get('/api/movies', getMovies);
-app.get('/api/movies/:id', (req, res) => {
-    res.send(req.params);
-})
+app.post("/api/movies", addMovie);
 
-app.listen(3000,()=>{
-    console.log('Listening on port:3000....')
-}); 
+app.put("/api/movies/:id", updateMovie);
+
+app.delete("/api/movies/:id", deleteMovie);
+
+
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}....`);
+});
